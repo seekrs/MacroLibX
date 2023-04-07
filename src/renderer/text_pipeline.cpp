f6c6b5e018a6705db5473bf52c7497f91c1ecb00
kbz_8 <kbz_8.dev@akel-engine.com>
2023-04-07 13:47:22 +0200

    adding stb_truetype

--- src/renderer/text_pipeline.cpp
+++ src/renderer/text_pipeline.cpp
@@ -6,17 +6,20 @@
 /*   By: maldavid <kbz_8.dev@akel-engine.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 16:41:13 by maldavid          #+#    #+#             */
-/*   Updated: 2023/04/06 16:42:10 by maldavid         ###   ########.fr       */
+/*   Updated: 2023/04/07 00:13:50 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <renderer/text_pipeline.h>
 
+#define STB_TRUETYPE_IMPLEMENTATION
+#include <std_truetype.h>
+
 namespace mlx
 {
 	void TextPutPipeline::init(Renderer& renderer) noexcept
 	{
-
+		
 	}
 
 	void TextPutPipeline::put(int x, int y, int color, std::string str)
