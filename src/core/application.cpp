7f7d197e4232ada02375497ecdb411196de76ad1
Kbz-8 <kbz_8.code@proton.me>
2023-08-28 10:53:22 +0200

    adding SDL initialisation and quit, improving inputs code readability

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -6,7 +6,7 @@
 /*   By: maldavid <kbz_8.dev@akel-engine.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 22:10:52 by maldavid          #+#    #+#             */
-/*   Updated: 2023/04/25 15:12:57 by maldavid         ###   ########.fr       */
+/*   Updated: 2023/08/28 10:19:53 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 #include <renderer/images/texture.h>
 #include <renderer/core/render_core.h>
 #include <array>
+#include <core/errors.h>
 #include <utils/endian.h>
 
 namespace mlx::core
 {
+	Application::Application() : _in(std::make_unique<Input>())
+	{
+		if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_TIMER) != 0)
+			error::report(e_kind::fatal_error, "SDL error : unable to init all subsystems : %s", SDL_GetError());
+	}
+
 	void Application::run() noexcept
 	{
 		while(_in->is_running())
@@ -51,5 +58,10 @@ namespace mlx::core
         vkDeviceWaitIdle(Render_Core::get().getDevice().get());
 		Texture* texture = static_cast<Texture*>(ptr);
 		texture->destroy();
+	}
+
+	Application::~Application()
+	{
+		SDL_Quit();
 	}
 }
