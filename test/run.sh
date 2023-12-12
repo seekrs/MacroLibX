c5752f773ed9fc48611e679fa32e94eab778402f
Kbz-8 <kbz_8.dev@akel-engine.com>
2023-12-12 18:12:41 +0100

    fixing CI

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-if [ $(uname -s) = 'Darwin' ]; then
-	clang main.c ../libmlx.dylib -L /opt/homebrew/lib -lSDL2 -g && ./a.out;
-else
-	clang main.c ../libmlx.so -lSDL2 -g && ./a.out;
-fi
+bash ./build.sh
+./a.out
