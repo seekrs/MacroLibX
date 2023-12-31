d515ca5333fd7d832d4c0233b5f5d382579ef11b
Kbz-8 <kbz_8.dev@akel-engine.com>
2023-12-31 13:41:09 +0100

    debugging CI

--- scripts/fetch_dependencies.sh
+++ scripts/fetch_dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Update volk
-rm ../third_party/volk.*
+rm -f ../third_party/volk.c
+rm -f ../third_party/volk.h
 wget https://api.github.com/repos/zeux/volk/zipball/1.3.270 -O volk.zip
 unzip -o volk.zip -d ../third_party/
 mv ../third_party/zeux-volk*/volk.h ../third_party
@@ -11,6 +12,7 @@ rm volk.zip
 
 # Update Vulkan headers
 rm -rf ../third_party/vulkan
+rm -rf ../third_party/vk_video
 wget https://github.com/KhronosGroup/Vulkan-Headers/archive/main.zip -O vulkan-headers.zip
 unzip -o vulkan-headers.zip -d ../third_party/
 mv ../third_party/Vulkan-Headers-main/include/vulkan ../third_party/
