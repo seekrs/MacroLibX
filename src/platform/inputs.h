0acb08681971a5bb145119b3056abce1644435ae
Namonay <thek7995@gmail.com>
2024-01-09 01:37:01 +0100

    added back not so much 'unnecesary' include

--- src/platform/inputs.h
+++ src/platform/inputs.h
@@ -6,10 +6,11 @@
 /*   By: maldavid <kbz_8.dev@akel-engine.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 16:27:35 by maldavid          #+#    #+#             */
-/*   Updated: 2023/12/11 19:47:20 by vavaas           ###   ########.fr       */
+/*   Updated: 2023/12/11 19:47:20 by vavaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <array>
 #include <memory>
 #include <vector>
 #include <cstdint>
