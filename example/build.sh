9024b9cc12fcb70fccea6384f962a65ad0d87417
Namonay <thek7995@gmail.com>
2024-01-12 04:25:12 +0100

    upgraded example to test all mlx functions

--- example/build.sh
+++ example/build.sh
@@ -7,6 +7,6 @@ fi
 if [ $(uname -s) = 'Darwin' ]; then
 	clang main.c ../libmlx.dylib -L /opt/homebrew/lib -lSDL2 -g;
 else
-	clang main.c ../libmlx.so -lSDL2 -g;
+	clang main.c ../libmlx.so -lSDL2 -g -Wall -Wextra -Werror;
 fi
 
