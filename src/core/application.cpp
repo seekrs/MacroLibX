5af4c82c1b1ec9e2b874bd1b8d87e10da062d6cb
Kbz-8 <kbz_8.dev@akel-engine.com>
2024-04-22 17:35:41 +0200

    fixing multiple window issue

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -6,7 +6,7 @@
 /*   By: maldavid <kbz_8.dev@akel-engine.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 22:10:52 by maldavid          #+#    #+#             */
-/*   Updated: 2024/04/21 18:13:29 by maldavid         ###   ########.fr       */
+/*   Updated: 2024/04/22 17:35:15 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ namespace mlx::core
 
 	void Application::run() noexcept
 	{
+		_in->run();
 		while(_in->isRunning())
 		{
 			if(!_fps.update())
