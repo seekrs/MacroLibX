5b726fe74aa61ea785b2946d85d95fc86a23588c
Kbz-8 <kbz_8.code@proton.me>
2024-12-15 03:53:56 +0100

    adding missing function implementation

--- runtime/Includes/Platform/Window.h
+++ runtime/Includes/Platform/Window.h
@@ -13,14 +13,21 @@ namespace mlx
 			Window(const mlx_window_create_info* info, bool hidden = false);
 
 			inline Handle GetWindowHandle() const noexcept { return p_window; }
-			inline int GetWidth() const noexcept { return m_width; }
-			inline int GetHeight() const noexcept { return m_height; }
+			inline int GetWidth() noexcept { SDLManager::Get().GetWindowSize(p_window, &m_width, &m_height); return m_width; }
+			inline int GetHeight() noexcept { SDLManager::Get().GetWindowSize(p_window, &m_width, &m_height); return m_height; }
 			inline std::uint32_t GetID() const noexcept { return m_id; }
 			inline const std::string& GetName() const { return m_name; }
 
 			inline void MoveMouse(int x, int y) { SDLManager::Get().MoveMouseOnWindow(p_window, x, y); }
 			inline void GetScreenSizeWindowIsOn(int* x, int* y) { SDLManager::Get().GetScreenSizeWindowIsOn(p_window, x, y); }
+
 			inline void SetPosition(int x, int y) { SDLManager::Get().SetWindowPosition(p_window, x, y); }
+			inline void SetSize(int x, int y) { SDLManager::Get().SetWindowSize(p_window, x, y); m_width = x; m_height = y; }
+			inline void SetTitle(std::string title) { SDLManager::Get().SetWindowTitle(p_window, title); m_name = std::move(title); }
+			inline void SetFullscreen(bool enable) { SDLManager::Get().SetWindowFullscreen(p_window, enable); }
+
+			inline void GetPosition(int* x, int* y) { SDLManager::Get().GetWindowPosition(p_window, x, y); }
+			inline void GetSize(int* x, int* y) { *x = GetWidth(); *y = GetHeight(); }
 
 			inline VkSurfaceKHR CreateVulkanSurface(VkInstance instance) const noexcept { return SDLManager::Get().CreateVulkanSurface(p_window, instance); }
 			inline std::vector<const char*> GetRequiredVulkanInstanceExtentions() const noexcept { return SDLManager::Get().GetRequiredVulkanInstanceExtentions(p_window); }
