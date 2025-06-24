26eab93f9fefa8e292fcfe55eb8448c5ca5a680b
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-06-24 07:10:02 +0200

    starting to work on headless support

--- runtime/Sources/Core/UUID.cpp
+++ runtime/Sources/Core/UUID.cpp
@@ -7,7 +7,14 @@ namespace mlx
 	static std::random_device random_device;
 	static std::mt19937_64 engine(random_device());
 	static std::uniform_int_distribution<std::uint64_t> uniform_distribution;
+	static std::unordered_set<std::uint64_t> registry;
 
-	UUID::UUID() : m_uuid(uniform_distribution(engine)) {}
-	UUID::UUID(std::uint64_t uuid) : m_uuid(uuid) {}
+	UUID::UUID()
+	{
+		do
+		{
+			m_uuid = uniform_distribution(engine);
+		} while(registry.contains(m_uuid));
+		registry.emplace(m_uuid);
+	}
 }
